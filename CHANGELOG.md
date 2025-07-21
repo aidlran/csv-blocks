be595df752a4d41047680bc6508ac9b5b743c025
Aidan <86127892+aidlran@users.noreply.github.com>
2025-07-21 13:40:29 +0100

    refactor: program error catch

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## Unreleased
+
+- Added a program wide error catch.
+
+## [1.0.0](https://github.com/aidlran/csv-blocks/releases/tag/v1.0.0) - 2025-07-20
+
+- Initial release.
--- src/cli.mjs
+++ src/cli.mjs
@@ -5,60 +5,60 @@ import { createReadStream, statSync, writeFileSync } from 'fs';
 import pkg from '../package.json' with { type: 'json' };
 import { csvb } from './lib.mjs';
 
-const program = new Command(pkg.name)
-  .description(pkg.description)
-  .version(pkg.version, '-v, --version')
-  .description('Parses a CSV blocks formatted file, checks for errors, and outputs records.')
-  .argument('<input>', "Path to input file. Pass '-' to use stdin.")
-  .argument('[output]', "Path to output file. Pass '-' to use stdout.")
-  .option('--no-print', "Don't print the table.")
-  .action((input, output, { print }) => {
-    /** @type {import('stream').Stream} */
-    let inputStream;
+const program = new Command(pkg.name);
 
-    if (input === '-') {
-      if (process.stdin.isTTY) {
-        program.error('stdin provided no input');
-      }
-      inputStream = process.stdin;
-    } else {
-      try {
-        const stat = statSync(input);
-        if (!stat.isFile()) {
+try {
+  program
+    .description(pkg.description)
+    .version(pkg.version, '-v, --version')
+    .description('Parses a CSV blocks formatted file, checks for errors, and outputs records.')
+    .argument('<input>', "Path to input file. Pass '-' to use stdin.")
+    .argument('[output]', "Path to output file. Pass '-' to use stdout.")
+    .option('--no-print', "Don't print the table.")
+    .action((input, output, { print }) => {
+      /** @type {import('stream').Stream} */
+      let inputStream;
+
+      if (input === '-') {
+        if (process.stdin.isTTY) {
+          program.error('stdin provided no input');
+        }
+        inputStream = process.stdin;
+      } else {
+        if (!statSync(input).isFile()) {
           program.error(`Not a file: '${input}'`);
         }
         inputStream = createReadStream(input);
-      } catch (e) {
-        // @ts-ignore
-        if (e instanceof Error && e?.code === 'ENOENT') {
-          program.error(e.message);
-        }
-        throw e;
       }
-    }
 
-    /** @type {object[]} */
-    const records = [];
+      /** @type {object[]} */
+      const records = [];
 
-    const stream = inputStream
-      .pipe(csvb())
-      .on('error', (e) => program.error(e.message))
-      .on('data', (record) => records.push(record));
+      const stream = inputStream
+        .pipe(csvb())
+        .on('error', (e) => program.error(e.message))
+        .on('data', (record) => records.push(record));
 
-    if (print) {
-      stream.on('end', () => console.table(records));
-    }
+      if (print) {
+        stream.on('end', () => console.table(records));
+      }
 
-    if (typeof output === 'string') {
-      stream.on('end', () => {
-        const json = JSON.stringify(records, null, 2);
-        if (output === '-') {
-          process.stdout.write(json);
-        } else {
-          writeFileSync(output, json);
-        }
-      });
-    }
-  });
+      if (typeof output === 'string') {
+        stream.on('end', () => {
+          const json = JSON.stringify(records, null, 2);
+          if (output === '-') {
+            process.stdout.write(json);
+          } else {
+            writeFileSync(output, json);
+          }
+        });
+      }
+    });
 
-program.parse();
+  program.parse();
+} catch (e) {
+  if (e instanceof Error) {
+    program.error(e.message);
+  }
+  throw e;
+}
